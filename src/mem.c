8fdab0f6dc7af4ef67447a5cb0b32d742f919388
Marko Radosavljevic <marko.r463@gmail.com>
2024-10-21 23:27:27 +0200

    stack_end 0x3000

--- src/mem.c
+++ src/mem.c
@@ -34,7 +34,7 @@ SEG get_mem_seg(uint32_t address)
 		return KB;
 	}
 
-	if (address <= STACK_END) {
+	if (STACK_BEGIN < address && address <= STACK_END) {
 		return STACK;
 	}
 
@@ -69,7 +69,7 @@ uint64_t get_mem_addr(uint32_t address)
 		return (uint64_t)&key_pressed;
 	}
 
-	if (address < STACK_END) {
+	if (STACK_BEGIN < address && address <= STACK_END) {
 		return (uint64_t)mem[STACK] + (address - STACK_BEGIN);
 	}
 
